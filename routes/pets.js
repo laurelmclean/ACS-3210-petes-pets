231302bf19ff1164d6d0a2c50a96b60d977ce388
Laurel McLean <laurel.mclean@live.ca>
2023-05-22 16:16:56 -0600

    users can search pets by name and breed

--- routes/pets.js
+++ routes/pets.js
@@ -55,4 +55,19 @@ module.exports = (app) => {
       return res.redirect('/')
     });
   });
+
+  // SEARCH PET
+  // i modifier makes it case insenstive
+  // $or operator to match documents that have a name or species that matches the regular expression defined by term
+  app.get('/search', (req, res) => {
+    term = new RegExp(req.query.term, 'i')
+      Pet.find({
+        $or: [
+          { 'name': term },
+          { 'species': term }
+        ]
+      }).exec((err, pets) => {
+        res.render('pets-index', { pets: pets });
+      })
+  });
 }
