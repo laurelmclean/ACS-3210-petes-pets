b918ca432453c7377c7d64de4ffd82d0f7ca1f12
Laurel McLean <laurel.mclean@live.ca>
2023-05-23 19:48:44 -0600

    implemented pagination on the home page

--- models/pet.js
+++ models/pet.js
@@ -2,6 +2,12 @@
 
 const mongoose = require('mongoose'),
         Schema = mongoose.Schema;
+      
+const mongoosePaginate = require('mongoose-paginate');
+
+mongoosePaginate.paginate.options = {
+  limit: 3 // how many records on each page
+};
 
 const PetSchema = new Schema({
     name            : { type: String, required: true }
@@ -15,5 +21,7 @@ const PetSchema = new Schema({
 {
   timestamps: true
 });
+
+PetSchema.plugin(mongoosePaginate);
 
 module.exports = mongoose.model('Pet', PetSchema);
